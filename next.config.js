8a18d48fd552812364c7306f70718077f9517f90
azucena718 <azucena718@accudata.com.tw>
2024-04-11 17:05:56 +0800

    update: build useMap hook

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   env: {
     client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
     client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+    google_map: process.env.NEXT_PUBLIC_GOOGLE_MAP,
   },
 }
 
