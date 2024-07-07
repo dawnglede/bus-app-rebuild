d63ff9b8bbcdaec591aba8fca5fd2eee1598d597
dawnglede <50606676+dawnglede@users.noreply.github.com>
2024-07-07 18:38:22 +0800

    feat: add useMap in busRoute page

--- src/app/[locale]/busRoute/page.tsx
+++ src/app/[locale]/busRoute/page.tsx
@@ -1,9 +1,16 @@
 'use client'
+import useMap from '@/hooks/useMap'
 
 export default function BusRoute() {
+  const getMap = useMap({
+    locations: [
+      { lat: 25.033964, lng: 121.564472 },
+      { lat: 25.0585, lng: 121.52014 }
+    ]
+  })
   return (
     <div>
-      <div id='map'></div>
+      <div id='map' className='h-[100vh] w-[100%]'></div>
     </div>
   )
 }
