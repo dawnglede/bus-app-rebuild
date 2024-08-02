5f2bcac384d8ed24d632c24205105656dd3f3c21
dawnglede <50606676+dawnglede@users.noreply.github.com>
2024-08-02 11:55:38 +0800

    feat: add api for feching bus infomation

--- src/app/api/getArrivalTime/route.ts
+++ src/app/api/getArrivalTime/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 async function getValidToken(origin: string) {
   const tokenResponse = await fetch(`${origin}/api/getToken`)
   const tokenData = await tokenResponse.json()
-  // TODO: extract this function as independent function and add set cookie
+
   return tokenData.access_token
 }
 
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
   const { city, routeUID } = await req.json()
   try {
     res = await fetch(
-      `https://tdx.transportdata.tw/api/basic/v2/Bus/StopOfRoute/City/${city}?%24filter=RouteUID%20eq%20%27${routeUID}%27&%24top=30&%24format=JSON`,
+      `https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/${city}?%24filter=RouteUID%20eq%20%27${routeUID}%27&%24top=30&%24format=JSON`,
       {
         method: 'GET',
         headers: {
@@ -27,7 +27,7 @@ export async function POST(req: Request) {
       },
     )
     if (!res.ok) {
-      throw new Error('Fail to fetch stop info, status code :' + res.status)
+      throw new Error('Fail to fetch arrival time, status code :' + res.status)
     }
     const data = await res.json()
     return NextResponse.json({ data, success: true })
