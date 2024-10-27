dfcee4e0b96801a7a7edcd32e9cd5aace9c44ee4
dawnglede <50606676+dawnglede@users.noreply.github.com>
2024-10-27 23:52:20 +0800

    refactor: refactor useLocation hook

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -4,16 +4,31 @@ import useStore from '@/store/useStore'
 export default function useLocation() {
   const [position, setPosition] = useState<GeolocationCoordinates>()
   const [isLocationAllowed, setIsLocationAllowed] = useState<boolean>(false)
+  const [watchId, setWatchId] = useState<number | null>(null)
   const { setGeolocationAvailable } = useStore()
 
-  useEffect(() => {
-    const geolocation: Geolocation = navigator.geolocation
-    let watchId: number
-    const watchGeoPosition = () => {
-      watchId = geolocation.watchPosition((position: GeolocationPosition) =>
+  const getCurrentPosition = () => {
+    const geolocation: Geolocation = navigator?.geolocation
+    let newPosition: GeolocationCoordinates
+    return new Promise<GeolocationCoordinates>((resolve) => {
+      geolocation.getCurrentPosition((position: GeolocationPosition) => {
+        newPosition = position.coords
+        setPosition(newPosition)
+        resolve(newPosition)
+      })
+    })
+  }
+  const watchGeoPosition = () => {
+    const geolocation: Geolocation = navigator?.geolocation
+    setWatchId(
+      geolocation.watchPosition((position: GeolocationPosition) =>
         setPosition(position.coords),
-      )
-    }
+      ),
+    )
+  }
+  useEffect(() => {
+    const geolocation: Geolocation = navigator?.geolocation
+
     if ('geolocation' in navigator) {
       setGeolocationAvailable(true)
     } else {
@@ -22,16 +37,18 @@ export default function useLocation() {
     }
     navigator.permissions.query({ name: 'geolocation' }).then((result) => {
       if (result.state === 'granted') {
-        watchGeoPosition()
         setIsLocationAllowed(true)
+        getCurrentPosition()
       } else if (result.state === 'prompt') {
-        geolocation.getCurrentPosition(() => {})
+        getCurrentPosition()
       } else if (result.state === 'denied') {
         setGeolocationAvailable(false)
       }
     })
-    return () => geolocation.clearWatch(watchId)
+    return () => {
+      if (watchId) geolocation.clearWatch(watchId)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLocationAllowed])
-  return { position, isLocationAllowed }
+  return { position, isLocationAllowed, getCurrentPosition, watchGeoPosition }
 }
